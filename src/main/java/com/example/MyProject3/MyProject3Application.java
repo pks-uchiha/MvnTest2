aa68531a1418b37443645d5f84b20fd0e9dd3c89
Piyush Kumar Singh <41823807+pks-uchiha@users.noreply.github.com>
2022-07-21 11:11:19 +0530

    init commit

---
+++ src/main/java/com/example/MyProject3/MyProject3Application.java
@@ -0,0 +1,14 @@
+package com.example.MyProject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject3Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(MyProject3Application.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
